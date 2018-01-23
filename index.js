d8b8d22d29f8ccf15962886a4a7a0abf189a82e5
Ebenezer Koodi <mrrkoodi@gmail.com>
2018-01-23 21:52:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
   }
 
 // Add your changeCompletely() function here:
-  function changeCompletely() {
-    
+  function changeCompletely(elements, index,array) {
+      array[index] = Math.floor(Math.random()*100 + 2).toString() + `${array[index]}s!!!`;
   }
